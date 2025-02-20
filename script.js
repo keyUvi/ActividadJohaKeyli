6bbc0e7c9a1565c48aa21be00fca5abee6774848
Mnjvar <daniel.menjivar2024@hilasaL.superate.org.sv>
2025-02-20 09:26:06 -0600

    first list:construction:

---
+++ script.js
@@ -0,0 +1,5 @@
+let manual = document.getElementById("contenido");
+
+let lista = document.createElement("li");
+lista.innerHTML = ("1. No procrastinar, 2.Estudiar");
+document.getElementById("contenido").appendChild(lista)
